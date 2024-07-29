a27502c5cb50bd37ae3f06644d304035399e27e9
KuromiDev <AliceDevelopments@proton.me>
2024-07-29 14:53:46 +0100

    updated contact email to be with custom domain redirect

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 function Redirect() {
-    window.open("https://mail.google.com/mail/u/0/?view=cm&fs=1&to=alicebe7276@proton.me&su=Enquiry Regarding Alice Developments", "_blank");
+    window.open("https://mail.google.com/mail/u/0/?view=cm&fs=1&to=enquiry@alicedevelopments.com&su=Enquiry Regarding Alice Developments", "_blank");
     }
 
 
